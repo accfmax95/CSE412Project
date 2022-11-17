c78f14e5db718a1b90e9cb224e1f924ff11fc26c
bjmorten-at-322225216192 <bjmorten@asu.edu>
2022-11-17 15:10:45 -0700

    test4

--- index.js
+++ index.js
@@ -11,8 +11,8 @@ const app = express();
 app.get("/", function (req, res) {
     res.sendFile(__dirname + "/index.html");
 });
-app.listen(3000, function () {
-    console.log("Server is running on localhost3000");
+app.listen(process.env.PORT || 3000, function () {
+    console.log("Server is running on localhost3000 or heroku port");
 });
 
 // const { Client } = require("pg");
